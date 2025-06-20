7d3d78693639f3ad6452667e3e7d9dd8e1de7395
Stas <ragioniere1980@gmail.com>
2025-06-20 17:27:52 +0300

    init

---
+++ WeattherForecast/WeatherForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeattherForecastApp.swift
+//  WeattherForecast
+//
+//  Created by Станислав Леонов on 20.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
